d26d87d9fa002d4797936bb71ce333a242b120c3
Bárbara Broto <barbara.broto97@gmail.com>
2022-08-10 12:04:07 +0200

    Creación del proyecto

---
+++ C4-T24/src/main/java/com/example/demo/C4T24Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C4T24Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(C4T24Application.class, args);
+	}
+
+}
